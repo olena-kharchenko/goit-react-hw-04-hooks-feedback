4e7f36f240fec163feecf9281373788cd72d30e5
olena-kharchenko <harchenko.es1990@gmail.com>
2020-12-19 19:35:09 +0200

    refactor to hooks

--- src/App.js
+++ src/App.js
@@ -1,60 +1,68 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './components/Section';
 import FeedbackOptions from './components/FeedbackOptions';
 import Statistics from './components/Statistics';
 import Notification from './components/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedback = { good, neutral, bad };
+
+  const setActiveItem = item => {
+    switch (item) {
+      case 'good':
+        return setGood(good + 1);
+
+      case 'neutral':
+        return setNeutral(neutral + 1);
+
+      case 'bad':
+        return setBad(bad + 1);
+
+      default:
+        throw new Error('Something went wrong!');
+    }
   };
 
-  setActiveItem = item => {
-    this.setState(PrevState => ({
-      [item]: PrevState[item] + 1,
-    }));
-  };
-
-  getNameOfItem = name => {
+  const getNameOfItem = name => {
     return `${name.slice(0, 1).toUpperCase()}${name.slice(1).toLowerCase()}`;
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((acc, item) => acc + item, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / countTotalFeedback());
   };
 
-  render() {
-    return (
-      <div className="wrapper">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.setActiveItem}
-            getNameOfItem={this.getNameOfItem}
+  return (
+    <div className="wrapper">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={feedback}
+          onLeaveFeedback={setActiveItem}
+          getNameOfItem={getNameOfItem}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="No feedback given"></Notification>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="No feedback given"></Notification>
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default App;
